8bff49faebd2ea480abefd63b35d1706daa30601
Angie Barrantes <angiefiorellacat@gmail.com>
2017-04-19 11:48:12 -0500

    Adding styles for the responsive design

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
   masonry();
 });
 
+$('#js-menu').click(menuMobile);
+$('.triangle').click(smoothScroll);
+$(window).scroll(backgroundMenu);
+var coverHeight = $('#js-cover-height').height();
+
+
 function masonry() {
   $('.grid').masonry({
     itemSelector: '.grid-item',
@@ -11,9 +17,24 @@ function masonry() {
   });
 }
 
-$('#js-menu').click(menuMobile);
-
 function menuMobile () {
 	$('header').toggleClass('view-nav-mobile');
+}
+
+function smoothScroll() {
+  $('html, body').animate({
+    scrollTop: $('#first-section').offset().top },
+    'slow');
+}
+
+function backgroundMenu() {
+  var scrollWindow = $(window).scrollTop();
+  console.log(scrollWindow);
+
+  if (scrollWindow >= coverHeight) {
+    $('.background-menu').addClass('dark');
+  } else if (scrollWindow < coverHeight) {
+    $('.background-menu').removeClass('dark');
+  }
 }
 
